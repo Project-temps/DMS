c132377df2af79f8bffca1245307be63d63e9dd8
Project-temps <workacctemp@gmail.com>
2025-06-22 14:49:23 +0200

    Fix syntax in api/urls.py: close urlpatterns list

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path
 from .views import PredictionAPIView, CalculateTHIAPIView
 
-urlpatterns = [
-    path('predict/', PredictionAPIView.as_view(), name='predict-api'),
-from .views import CalculateTHIAPIView
-# from .views import PredictionAPIView
 
 urlpatterns = [
     # path('predict/', PredictionAPIView.as_view(), name='predict-api'),
