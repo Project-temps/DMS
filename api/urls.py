762e5d89161f7121412904ac77e49b4f285a54d4
Project-temps <workacctemp@gmail.com>
2025-06-22 11:51:50 +0200

    New ui first version

---
+++ api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import CalculateTHIAPIView
+# from .views import PredictionAPIView
+
+urlpatterns = [
+    # path('predict/', PredictionAPIView.as_view(), name='predict-api'),
+    path('calculate_thi/', CalculateTHIAPIView.as_view(), name='calculate-thi-api'),
+]
---
+++ myproject/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('ui.urls')),  # Route for the main UI
+    path('auth/', include('authentication.urls')),
+    path('django_plotly_dash/', include('django_plotly_dash.urls')),
+    path('api/', include('api.urls')),
+]
