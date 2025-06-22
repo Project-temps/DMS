ac46c37d0816ab8864088145e9207fa7b6e44b9c
Project-temps <workacctemp@gmail.com>
2025-06-22 21:30:14 +0200

    Add unit tests and keras stub

--- ui/tests.py
+++ ui/tests.py
@@ -1,1 +1,17 @@
 from django.test import TestCase
+from .views import calculate_statistics
+
+class HomePageTests(TestCase):
+    def test_index_page_loads(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+class StatisticsFunctionTests(TestCase):
+    def test_calculate_statistics(self):
+        chart_data = [
+            {'name': 'Farm1', 'values_temp': [1, 3, 5]},
+        ]
+        stats = calculate_statistics(chart_data)
+        self.assertEqual(stats['Farm1']['values_temp']['min'], 1)
+        self.assertEqual(stats['Farm1']['values_temp']['max'], 5)
+        self.assertEqual(stats['Farm1']['values_temp']['mean'], 3)
