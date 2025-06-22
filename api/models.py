1a628cc21e7b50006d9896e2f16fd6bcdde714a8
Project-temps <workacctemp@gmail.com>
2025-06-22 16:40:25 +0200

    Remove boilerplate Django comments

--- api/models.py
+++ api/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Farm(models.Model):
     name = models.CharField(max_length=100, unique=True)
     country = models.CharField(max_length=80)
